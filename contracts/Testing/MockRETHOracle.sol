65ad6794d59fe3d7e8c80ade720124754e4f8005
teddy-clapp <teddy@quadrata.io>
2023-05-19 11:09:23 -0700

    Create MockRETHOracle.sol

---
+++ contracts/Testing/MockRETHOracle.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.17;
+
+// SPDX-License-Identifier: GPL v3
+
+contract MockRETHOracle {
+
+    uint private _price = 1.0719 ether;
+
+    function setPrice(uint price) public {
+        _price = price;
+    }
+
+    function getPrice() public view returns (uint) {
+        return _price;
+    }
+}
