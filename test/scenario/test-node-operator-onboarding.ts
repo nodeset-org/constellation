d1355ee0dc044606067f1c35a4174f4af70280c7
teddy-clapp <teddy@quadrata.io>
2023-05-31 17:16:00 -0700

    Create test-node-operator-onboarding.ts

---
+++ test/scenario/test-node-operator-onboarding.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { protocolFixture, SetupData } from "../test";
+import { BigNumber as BN } from "ethers";
+
+
+describe("Node Operator Onboarding", function () {
+
+});
