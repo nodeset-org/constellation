73271c98f989e998adc7013bd89e138d8a5e74fc
teddy-clapp <teddy@quadrata.io>
2023-07-26 00:31:12 +0300

    delete tests for removed feats

--- test/test-xrEth.ts
+++ test/test-xrEth.ts
@@ -5,12 +5,6 @@ import { protocolFixture, SetupData } from "./test";
 import { BigNumber } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 
-export async function mint_xrEth(setupData: SetupData, from: SignerWithAddress, amount: BigNumber) {
-  const { protocol, signers } = setupData;
-
-  return from.sendTransaction({ to: protocol.xrETH.address, value: amount, gasLimit: 1000000 });
-}
-
 describe("xrETH", function () {
 
   // add tests for deposit and withdraw
