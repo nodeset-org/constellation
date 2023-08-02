69be0e3ed60625dbfcb46c3d6d65c910f5bb196d
teddy-clapp <teddy@quadrata.io>
2023-08-02 15:41:16 -0700

    Update PriceFetcher.sol

--- contracts/PriceFetcher.sol
+++ contracts/PriceFetcher.sol
@@ -7,6 +7,10 @@ import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "./UpgradeableBase.sol";
 
 contract PriceFetcher is UpgradeableBase {
+
+
+    /// @notice Returns the current price of ETH in RPL
+    /// @return The price of ETH in RPL with 18 decimals
     function getPrice() public view returns (uint256) {
         IUniswapV3Pool pool = IUniswapV3Pool(
             getDirectory().UNISWAP_RPL_ETH_POOL_ADDRESS()
