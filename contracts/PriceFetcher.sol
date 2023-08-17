4a987bf1175aac792d74ffc9a1e676daf2d3e897
teddy-clapp <teddy@quadrata.io>
2023-08-16 17:25:30 -0700

    update

--- contracts/PriceFetcher.sol
+++ contracts/PriceFetcher.sol
@@ -3,19 +3,20 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+//import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 
 import "./UpgradeableBase.sol";
 import "./Utils/Constants.sol";
 
 contract PriceFetcher is UpgradeableBase {
-    /// @notice Returns the current price of ETH in RPL
+    /// @notice Returns the average price of ETH denominated in RPL with 18 decimals from 45 to 60 minutes ago
     /// @return The price of ETH denominated in RPL with 18 decimals
     function getPrice() public view returns (uint256) {
         IUniswapV3Pool pool = IUniswapV3Pool(
             Constants.UNISWAP_RPL_ETH_POOL_ADDRESS
         );
         (uint160 sqrtPriceX96, , , , , , ) = pool.slot0();
-        uint256 price = (uint256(sqrtPriceX96)**2) * 1e7 / (2 ** 192);
+        uint256 price = ((uint256(sqrtPriceX96) ** 2) * 1e7) / (2 ** 192);
         return price * 1e11;
     }
 }
