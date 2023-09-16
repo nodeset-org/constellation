75b103a365133699d580a285db2acb659b4e49c1
teddy-clapp <teddy@quadrata.io>
2023-09-15 21:57:37 -0700

    Update MockUniswapV3Pool.sol

--- contracts/Testing/Mocks/MockUniswapV3Pool.sol
+++ contracts/Testing/Mocks/MockUniswapV3Pool.sol
@@ -7,7 +7,7 @@ contract MockUniswapV3Pool {
     uint256 public sqrtPriceX96;
 
     constructor() {
-        sqrtPriceX96 = 566508141052302349635711194631; // value from mainnet at some point in time
+        sqrtPriceX96 = 566508141052302349635711194631; // value from mainnet Jan-13-2023 ~51rpl/eth
     }
 
     function setSqrtPriceX96(uint256 _sqrtPriceX96) external {
