5687f62cff8e0a03b176cd6ae3f594b6f6a4d892
Shawn M <100187735+TheAlpha0ne@users.noreply.github.com>
2023-11-06 03:22:41 -0700

    Create get-contract-addresses.sh

---
+++ scripts/get-contract-addresses.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SECRET_NAMESPACE="dev/goerli"
+
+# List all secrets within the specified namespace
+secrets_list=$(aws secretsmanager list-secrets --query "SecretList[?starts_with(Name, '$SECRET_NAMESPACE/')].[Name]" --output text)
+
+# Loop through the list of secret names and retrieve their values
+for secret_name in $secrets_list; do
+  secret_value=$(aws secretsmanager get-secret-value --secret-id "$secret_name" --query "SecretString" --output text)
+  echo "Secret Name: $secret_name"
+  echo "Secret Value: $secret_value"
+done
