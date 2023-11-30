c7b1e8a4527c1129309b0d2849615238b84a3ffb
Shawn M <100187735+TheAlpha0ne@users.noreply.github.com>
2023-11-30 02:44:34 -0700

    Update publish-contract-addresses.sh

--- scripts/publish-contract-addresses.sh
+++ scripts/publish-contract-addresses.sh
@@ -3,20 +3,12 @@ DATA_FILE="../dist/contract_addresses.data"
 SECRET_NAMESPACE="dev/goerli"
 BUCKET_NAME="constellation"
 BUCKET_DIR="contract_addresses"
-
-# Check if S3 bucket exists
-if ! aws s3 ls "s3://$BUCKET_NAME" > /dev/null 2>&1 ; then
-  # Create the S3 bucket if it doesn't exist
-  aws s3 mb "s3://$BUCKET_NAME"
+if ! aws s3 ls "s3://$BUCKET_NAME" > /dev/null 2>&1; then
+    "Creating constellation data bucket"
+    aws s3 mb "s3://$BUCKET_NAME"
 fi
-
-# Upload the file to the S3 bucket
-aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/"
-
-# Read each line from the data file
+aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/$BUCKET_DIR/"
 while IFS= read -r line; do
-  key="${line%%=*}"
-  value="${line#*=}"
-  # Create a secret in AWS Secrets Manager with the key as the secret name and the value as the secret value
-  aws secretsmanager create-secret --name "$SECRET_NAMESPACE/$key" --secret-string "$value" --overwrite
+  key="${line%%:*}"
+  aws secretsmanager create-secret --name "$SECRET_NAMESPACE/$key" --secret-string "$line" --overwrite
 done < "$DATA_FILE"
