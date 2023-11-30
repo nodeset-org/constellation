367a21a5b2335963acfe94119c47e15b6b9cdfc2
Shawn M <100187735+TheAlpha0ne@users.noreply.github.com>
2023-11-30 01:19:33 -0700

    Update publish-contract-addresses.sh

--- scripts/publish-contract-addresses.sh
+++ scripts/publish-contract-addresses.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 DATA_FILE="../dist/contract_addresses.data"
 SECRET_NAMESPACE="dev/goerli"
+BUCKET_NAME="constellation/contract_address"
 
+# Check if S3 bucket exists
+if ! aws s3 ls "s3://$BUCKET_NAME" > /dev/null 2>&1 ; then
+  # Create the S3 bucket if it doesn't exist
+  aws s3 mb "s3://$BUCKET_NAME"
+fi
+
+# Upload the file to the S3 bucket
+aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/"
+
+# Read each line from the data file
 while IFS= read -r line; do
   key="${line%%=*}"
   value="${line#*=}"
