af271f9f9d0440bc182915d7745c7464ca02140d
Shawn M <100187735+TheAlpha0ne@users.noreply.github.com>
2023-12-01 02:02:11 -0700

    Update publish-contract-addresses.sh

--- scripts/publish-contract-addresses.sh
+++ scripts/publish-contract-addresses.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-DATA_FILE="../dist/contract_addresses.data"
+DATA_FILE="../dist/contracts.data"
 SECRET_NAMESPACE="dev/goerli"
-BUCKET_NAME="constellation"
-BUCKET_DIR="contract_addresses"
+BUCKET_NAME="constellation-metadata"
 if ! aws s3 ls "s3://$BUCKET_NAME" > /dev/null 2>&1; then
     "Creating constellation data bucket"
     aws s3 mb "s3://$BUCKET_NAME"
 fi
-aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/$BUCKET_DIR/"
+aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/"
 while IFS= read -r line; do
   key="${line%%:*}"
   aws secretsmanager create-secret --name "$SECRET_NAMESPACE/$key" --secret-string "$line" --overwrite
