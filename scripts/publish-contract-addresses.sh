6acd3dc957cb0e02d5f6952970ba6d1c32aa4812
Shawn M <100187735+TheAlpha0ne@users.noreply.github.com>
2023-12-01 02:25:01 -0700

    Update publish-contract-addresses.sh

--- scripts/publish-contract-addresses.sh
+++ scripts/publish-contract-addresses.sh
@@ -11,5 +11,10 @@ ls
 aws s3 cp "$DATA_FILE" "s3://$BUCKET_NAME/"
 while IFS= read -r line; do
   key="${line%%:*}"
-  aws secretsmanager create-secret --name "$SECRET_NAMESPACE/$key" --secret-string "$line" --overwrite
+  if aws secretsmanager describe-secret --secret-id "$SECRET_NAMESPACE/$key" > /dev/null 2>&1; then
+    aws secretsmanager update-secret --secret-id "$SECRET_NAMESPACE/$key" --secret-string "$line"
+  else
+    aws secretsmanager create-secret --name "$SECRET_NAMESPACE/$key" --secret-string "$line"
+  fi
 done < "$DATA_FILE"
+
