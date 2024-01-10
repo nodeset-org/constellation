a69c45329139a1cc6035f64106cf872b12d9434a
mryamz <teddy.clapp@gmail.com>
2024-01-09 18:21:42 -0800

    rm debug

--- test/test-operatorDistributor.ts
+++ test/test-operatorDistributor.ts
@@ -16,25 +16,19 @@ describe("Operator Distributor", function () {
 		const { operatorDistributor } = protocol;
 		const { rocketNodeStakingContract } = rocketPool;
 		const mockRocketNodeStaking = await ethers.getContractAt("RocketNodeStaking", rocketNodeStakingContract.address);
-		console.log("hi3");
 
 		await prepareOperatorDistributionContract(setupData, 2);
 		await registerNewValidator(setupData, [signers.random, signers.random2])
-		console.log("hi2");
 
 		// send rpl to the operator distributor
 		const rplAmount = ethers.utils.parseEther("1000");
 		await rocketPool.rplContract.connect(signers.rplWhale).transfer(operatorDistributor.address, rplAmount);
 
-		console.log("passes rplContract.connec")
 		const minipools: MockMinipool[] = await getMockMinipoolsInProtocol(setupData);
-		console.log("got mock miniipools")
 		for (let i = 0; i < minipools.length; i++) {
-			console.log("setting at i", i);
 			const minipool = minipools[i];
 			//await minipool.setNodeDepositBalance(ethers.utils.parseEther("1"));
 		}
-		console.log("hi1-----dfasdfasdfasdfas-dfa-----------------------");
 
 		const initialRplStake = await mockRocketNodeStaking.getNodeRPLStake(signers.random.address);
 		await operatorDistributor.connect(signers.protocolSigner).processNextMinipool();
