ddb3024eb716cce2861c199c7c3867e6f1f53e0d
mryamz <teddy@nodeset.io>
2024-01-30 20:11:00 -0800

    Update test-validator-account-factory.ts

--- test/test-validator-account-factory.ts
+++ test/test-validator-account-factory.ts
@@ -70,13 +70,9 @@ describe("Validator Account Factory", function () {
             expectedMinipoolAddress: depositData.minipoolAddress
         }
 
-        await protocol.validatorAccountFactory.connect(signers.hyperdriver).createNewValidatorAccount(config, nextAddress, {
+        await expect(protocol.validatorAccountFactory.connect(signers.hyperdriver).createNewValidatorAccount(config, nextAddress, {
             value: ethers.utils.parseEther("1")
-        })
-
-        expect(await protocol.directory.hasRole(ethers.utils.id("FACTORY_ROLE"), protocol.validatorAccountFactory.address)).equals(true)
-        expect(await protocol.directory.hasRole(ethers.utils.id("CORE_PROTOCOL_ROLE"), protocol.validatorAccountFactory.address)).equals(true)
-        expect(await protocol.directory.hasRole(ethers.utils.id("CORE_PROTOCOL_ROLE"), nextAddress)).equals(true)
+        })).to.be.revertedWith("Whitelist: Provided address is not an allowed operator!")
     });
 
     it("fails - bad predicted address", async () => {
