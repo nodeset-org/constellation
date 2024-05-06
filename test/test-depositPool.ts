06a5e6232bf5ccf2c47fd82ff5901f146a7ad7c8
mryamz <teddy@nodeset.io>
2024-05-06 11:00:42 -0700

    bad logic

--- test/test-depositPool.ts
+++ test/test-depositPool.ts
@@ -1,10 +1,11 @@
 import { expect } from "chai";
-import { ethers, network } from "hardhat";
+import { ethers, network, web3 } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { protocolFixture, SetupData } from "./test";
 import { BigNumber as BN } from "ethers";
 import { assertAddOperator, increaseEVMTime, prepareOperatorDistributionContract, registerNewValidator } from "./utils/utils";
+import { parseRewardsMap } from "./utils/merkleClaim";
 
 describe(`FundRouter`, () => {
 
@@ -151,7 +152,47 @@ describe(`FundRouter`, () => {
     })
 
     describe("merkleClaim", () => {
-        
+
+        interface Claims {
+            [address: string]: any;
+          }
+          
+
+        it("run proof", async () => {
+            const setupData = await loadFixture(protocolFixture);
+            const { protocol, signers, rocketPool: rp } = setupData;
+
+            await prepareOperatorDistributionContract(setupData, 1);
+            const [validator1, validator2] = await registerNewValidator(setupData, [signers.random, signers.random2]);
+
+            // Submit rewards snapshot
+            const rewards = [
+                {
+                    address: registeredNode1,
+                    network: 0,
+                    trustedNodeRPL: '0'.ether,
+                    nodeRPL: '1'.ether,
+                    nodeETH: '0'.ether
+                },
+                {
+                    address: registeredNode2,
+                    network: 0,
+                    trustedNodeRPL: '0'.ether,
+                    nodeRPL: '2'.ether,
+                    nodeETH: '0'.ether
+                }
+            ]
+
+            let treeData = parseRewardsMap(rewards);
+            let proof = (treeData.proof.claims as Claims)[`${validator.address}`];
+            let amountsRPL = [proof.amountRPL];
+            let amountsETH = [proof.amountETH];
+            let proofs = [proof.proof];
+
+
+            await protocol.depositPool.merkleClaim(validator.address, [0], amountsRPL, amountsETH, proof);
+        })
+
     })
 });
 
