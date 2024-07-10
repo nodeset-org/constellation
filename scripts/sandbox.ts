481a4035d251bf174fd54d37c14f4a0be6f00afe
mryamz <teddy@nodeset.io>
2024-07-10 11:29:39 -0700

    Update sandbox.ts

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -30,9 +30,6 @@ async function main() {
         return contract;
     });
 
-    const oracle = (await (await ethers.getContractFactory("MockRETHOracle")).deploy()) as IXRETHOracle;
-    console.log("oracle address", oracle.address)
-
     const sanctions = await retryOperation(async () => {
         const Sanctions = await ethers.getContractFactory("MockSanctions");
         const contract = await Sanctions.deploy();
@@ -41,27 +38,47 @@ async function main() {
     });
     console.log("sanctions address", sanctions.address);
 
-    const { directory } = await fastDeployProtocol(deployer, admin, rocketStorage.address, wETH.address, sanctions.address, uniswapV3Pool.address, oracle.address, admin.address, true);
+    const { directory } = await fastDeployProtocol(deployer, admin, rocketStorage.address, wETH.address, sanctions.address, uniswapV3Pool.address, admin.address, true);
 
-    // set adminServer to be ADMIN_SERVER_ROLE
-    const adminRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_SERVER_ROLE"));
-    await retryOperation(async () => {
-        await directory.connect(admin).grantRole(ethers.utils.arrayify(adminRole), deployer.address);
-    });
-
-    // set timelock to be TIMELOCK_ROLE
-    const timelockRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_SHORT"));
-    await retryOperation(async () => {
-        await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockRole), deployer.address);
-    });
-    console.log("timelock role set");
-
-    // set protocolSigner to be PROTOCOL_ROLE
-    const protocolRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("CORE_PROTOCOL_ROLE"));
-    await retryOperation(async () => {
-        await directory.connect(admin).grantRole(ethers.utils.arrayify(protocolRole), deployer.address);
-    });
-    console.log("protocol role set");
+      // set adminServer to be ADMIN_SERVER_ROLE
+      const adminRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_SERVER_ROLE"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(adminRole), deployer.address);
+      });
+  
+      // set adminServer to be ADMIN_ORACLE_ROLE
+      const adminOracleRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_ORACLE_ROLE"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(adminOracleRole), deployer.address);
+      });
+  
+      // set timelock to be TIMELOCK_ROLE
+      const timelockShortRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_SHORT"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockShortRole), deployer.address);
+      });
+      console.log("timelock short role set");
+  
+      // set timelock to be TIMELOCK_ROLE
+      const timelockMedRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_MED"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockMedRole), deployer.address);
+      });
+      console.log("timelock med role set");
+  
+      // set timelock to be TIMELOCK_ROLE
+      const timelockLongRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_LONG"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockLongRole), deployer.address);
+      });
+      console.log("timelock long role set");
+  
+      // set protocolSigner to be PROTOCOL_ROLE
+      const protocolRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("CORE_PROTOCOL_ROLE"));
+      await retryOperation(async () => {
+          await directory.connect(admin).grantRole(ethers.utils.arrayify(protocolRole), deployer.address);
+      });
+      console.log("protocol role set");
 }
 
 main()
