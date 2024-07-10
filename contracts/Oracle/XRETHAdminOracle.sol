614a8d0c70e5ab8b6702bcd72981b03f7858ce2a
teddy-nodeset <155682239+teddy-nodeset@users.noreply.github.com>
2024-07-10 13:40:33 -0700

    Oracle Merkle Claim Interface Merge + Hotfix Deployment Scripts (#205)
    * update
    
    * Update sandbox.ts

--- contracts/Oracle/XRETHAdminOracle.sol
+++ contracts/Oracle/XRETHAdminOracle.sol
@@ -4,15 +4,24 @@ import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 
 import '../Interfaces/Oracles/IXRETHOracle.sol';
 import '../UpgradeableBase.sol';
+import '../Operator/SuperNodeAccount.sol';
 
 pragma solidity 0.8.17;
 
-// interface def here
+struct MerkleProofParams {
+    address nodeAddress;
+    uint256[] rewardIndex;
+    uint256[] amountRPL;
+    uint256[] amountETH;
+    bytes32[][] merkleProof;
+}
 
 contract XRETHAdminOracle is IXRETHOracle, UpgradeableBase {
 
-    uint256 internal _lastUpdatedTotalYieldAccrued;
+    event TotalYieldAccruedUpdated(uint256 _amount);
+
     uint256 internal _totalYieldAccrued;
+    uint256 internal _lastUpdatedTotalYieldAccrued;
 
     constructor() initializer {}
 
@@ -26,7 +35,7 @@ contract XRETHAdminOracle is IXRETHOracle, UpgradeableBase {
         return _totalYieldAccrued;
     }
 
-    function setTotalYieldAccrued(bytes calldata _sig, uint256 _newTotalYieldAccrued, uint256 _sigTimeStamp) external {
+    function _setTotalYieldAccrued(bytes calldata _sig, uint256 _newTotalYieldAccrued, uint256 _sigTimeStamp) internal {
         address recoveredAddress = ECDSA.recover(
             ECDSA.toEthSignedMessageHash(
                 keccak256(abi.encodePacked(_newTotalYieldAccrued, _sigTimeStamp, address(this), block.chainid))
@@ -37,11 +46,33 @@ contract XRETHAdminOracle is IXRETHOracle, UpgradeableBase {
             _directory.hasRole(Constants.ADMIN_ORACLE_ROLE, recoveredAddress),
             'signer must have permission from admin oracle role'
         );
-        require(
-            _sigTimeStamp > _lastUpdatedTotalYieldAccrued,
-            'cannot update tya using old data'
-        );
+        require(_sigTimeStamp > _lastUpdatedTotalYieldAccrued, 'cannot update tya using old data');
         _totalYieldAccrued = _newTotalYieldAccrued;
         _lastUpdatedTotalYieldAccrued = block.timestamp;
+        emit TotalYieldAccruedUpdated(_newTotalYieldAccrued);
+    }
+
+    function setTotalYieldAccrued(
+        bytes calldata _sig,
+        uint256 _newTotalYieldAccrued,
+        uint256 _sigTimeStamp
+    ) external onlyAdmin {
+        _setTotalYieldAccrued(_sig, _newTotalYieldAccrued, _sigTimeStamp);
+    }
+
+    function setTotalYieldAccruedAndClaim(
+        MerkleProofParams calldata _merkleProofParams,
+        bytes calldata _sig,
+        uint256 _newTotalYieldAccrued,
+        uint256 _sigTimeStamp
+    ) external onlyAdmin {
+        _setTotalYieldAccrued(_sig, _newTotalYieldAccrued, _sigTimeStamp);
+        SuperNodeAccount(_directory.getSuperNodeAddress()).merkleClaim(
+            _merkleProofParams.nodeAddress,
+            _merkleProofParams.rewardIndex,
+            _merkleProofParams.amountRPL,
+            _merkleProofParams.amountETH,
+            _merkleProofParams.merkleProof
+        );
     }
 }
--- scripts/dev_setup.ts
+++ scripts/dev_setup.ts
@@ -3,7 +3,7 @@ import { getRocketPool, protocolFixture, protocolParams } from "../test/test";
 import { setDefaultParameters } from "../test/rocketpool/_helpers/defaults";
 import { deployRocketPool } from "../test/rocketpool/_helpers/deployment";
 import { getNextContractAddress } from "../test/utils/utils";
-import { IXRETHOracle, NodeAccountFactory } from "../typechain-types";
+import { IXRETHOracle } from "../typechain-types";
 import { expect } from "chai";
 import readline from 'readline';
 import { fastDeployProtocol, generateBytes32Identifier, retryOperation } from "./utils/deployment";
@@ -34,9 +34,6 @@ async function main() {
         return contract;
     });
 
-    const oracle = (await (await ethers.getContractFactory("MockRETHOracle")).deploy()) as IXRETHOracle;
-    console.log("oracle address", oracle.address)
-
     const sanctions = await retryOperation(async () => {
         const Sanctions = await ethers.getContractFactory("MockSanctions");
         const contract = await Sanctions.deploy();
@@ -45,7 +42,7 @@ async function main() {
     });
     console.log("sanctions address", sanctions.address);
 
-    const { directory } = await fastDeployProtocol(deployer, admin, rocketStorage.address, wETH.address, sanctions.address, uniswapV3Pool.address, oracle.address, admin.address, true);
+    const { directory } = await fastDeployProtocol(deployer, admin, rocketStorage.address, wETH.address, sanctions.address, uniswapV3Pool.address, admin.address, true);
 
     // set adminServer to be ADMIN_SERVER_ROLE
     const adminRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_SERVER_ROLE"));
@@ -53,12 +50,32 @@ async function main() {
         await directory.connect(admin).grantRole(ethers.utils.arrayify(adminRole), deployer.address);
     });
 
-    // set timelock to be TIMELOCK_ROLE
-    const timelockRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_SHORT"));
+    // set adminServer to be ADMIN_ORACLE_ROLE
+    const adminOracleRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_ORACLE_ROLE"));
     await retryOperation(async () => {
-        await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockRole), deployer.address);
+        await directory.connect(admin).grantRole(ethers.utils.arrayify(adminOracleRole), deployer.address);
     });
-    console.log("timelock role set");
+
+    // set timelock to be TIMELOCK_ROLE
+    const timelockShortRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_SHORT"));
+    await retryOperation(async () => {
+        await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockShortRole), deployer.address);
+    });
+    console.log("timelock short role set");
+
+    // set timelock to be TIMELOCK_ROLE
+    const timelockMedRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_MED"));
+    await retryOperation(async () => {
+        await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockMedRole), deployer.address);
+    });
+    console.log("timelock med role set");
+
+    // set timelock to be TIMELOCK_ROLE
+    const timelockLongRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TIMELOCK_LONG"));
+    await retryOperation(async () => {
+        await directory.connect(admin).grantRole(ethers.utils.arrayify(timelockLongRole), deployer.address);
+    });
+    console.log("timelock long role set");
 
     // set protocolSigner to be PROTOCOL_ROLE
     const protocolRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("CORE_PROTOCOL_ROLE"));
