f94970bbdbd3b164ea9047985d5376d830b2cdf3
Mike Leach (Wander) <6549498+VVander@users.noreply.github.com>
2024-08-01 19:05:25 -0500

    tests compile

--- test/test-xrEth.ts
+++ test/test-xrEth.ts
@@ -19,8 +19,7 @@ describe("xrETH", function () {
     expect(name).equals("Constellation ETH");
     expect(symbol).equals("xrETH");
     expect(await protocol.vCWETH.liquidityReserveRatio()).equals(ethers.utils.parseUnits("0.1", 5))
-    expect(await protocol.vCWETH.rplCoverageRatio()).equals(ethers.utils.parseUnits(".15", 18))
-    expect(await protocol.vCWETH.enforceRplCoverageRatio()).equals(false)
+    expect(await protocol.vCWETH.maxWethRplRatio()).equals(ethers.utils.parseUnits(".15", 18))
     expect(await protocol.vCWETH.treasuryFee()).equals(ethers.utils.parseUnits("0.01", 5))
     expect(await protocol.vCWETH.nodeOperatorFee()).equals(ethers.utils.parseUnits("0.01", 5))
   })
@@ -90,7 +89,7 @@ describe("xrETH", function () {
       const tvlCoverageRatio = ethers.utils.parseEther("0.1542069");
       await protocol.vCWETH.connect(signers.admin).setRplCoverageRatio(tvlCoverageRatio);
 
-      const tvlCoverageRatioFromContract = await protocol.vCWETH.rplCoverageRatio();
+      const tvlCoverageRatioFromContract = await protocol.vCWETH.maxWethRplRatio();
       expect(tvlCoverageRatioFromContract).equals(tvlCoverageRatio);
     });
 
