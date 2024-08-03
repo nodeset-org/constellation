52613c34da97482d2d049cc67d33d4b65bb666ae
mryamz <teddy@nodeset.io>
2024-08-02 21:08:08 -0700

    update

---
+++ test/Fees/test-fee-processing.ts
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { protocolFixture } from "../test";
+
+describe("Fees", async function () {
+
+    describe("When oracle is positive", async () => {
+        describe("When users redeem shares", async () => {
+
+        })
+
+        describe("When oracleError increases", async () => {
+
+        })
+
+        describe("When oracleError resets", async () => {
+
+        })
+    })
+
+    describe("When oracle is negative", async () => {
+        describe("When users redeem shares", async () => {
+
+        })
+
+        describe("When oracleError increases", async () => {
+
+        })
+
+        describe("When oracleError resets", async () => {
+
+        })
+    })
+
+})
