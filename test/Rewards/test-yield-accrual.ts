165c4c91c8d536f3e6ba20ef610e724813e26cd1
mryamz <teddy@nodeset.io>
2024-08-05 15:49:57 -0700

    add tests

--- test/Rewards/test-yield-accrual.ts
+++ test/Rewards/test-yield-accrual.ts
@@ -7,6 +7,10 @@ describe("Yield Accrual", function () {
 
     describe("When sender is not protocol", async () => {
         it("should revert", async () => {
+            try {
+                await loadFixture(protocolFixture);
+            } catch {}
+
             const { protocol, signers, rocketPool } = await loadFixture(protocolFixture);
 
             await expect(protocol.assetRouter.connect(signers.random5).onEthRewardsReceived(6, 6, 6)).to.be.revertedWith("Can only be called by Protocol!");
@@ -77,7 +81,33 @@ describe("Yield Accrual", function () {
     
                 describe("When community rewards are negative", async () => {
                     it("should revert", async () => {
-    
+                        const { protocol, signers, rocketPool } = await loadFixture(protocolFixture);
+                        const reward = ethers.utils.parseEther("1")
+                        const avgTreasuryFee = ethers.utils.parseEther(".9") // 90%
+                        const avgOperatorsFee = ethers.utils.parseEther(".9") // 90%
+
+                        expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(0);
+
+                        // simulate reward sent to asset router
+                        await protocol.assetRouter.connect(signers.protocolSigner).openGate();
+                        await signers.ethWhale.sendTransaction({
+                            to: protocol.assetRouter.address,
+                            value: reward.mul(10)
+                        })
+                        await protocol.assetRouter.connect(signers.protocolSigner).closeGate();
+
+                        expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(reward.mul(10));
+                        expect(await ethers.provider.getBalance(protocol.yieldDistributor.address)).equals(0);
+                        const initialBalanceTreasury = await ethers.provider.getBalance(await protocol.directory.getTreasuryAddress());
+                        expect(await protocol.assetRouter.balanceEthAndWeth()).equals(0);
+
+                        const expectedTreasuryPortion = reward.mul(avgTreasuryFee).div(ethers.utils.parseEther("1"))
+                        const expectedOperatorPortion = reward.mul(avgOperatorsFee).div(ethers.utils.parseEther("1"))
+                        const expectedCommunityPortion = reward.sub(expectedTreasuryPortion.add(expectedOperatorPortion));
+
+                        // expect the community rewards underflow to cause revert 
+                        await expect(protocol.assetRouter.connect(signers.protocolSigner).onEthRewardsReceived(reward, avgTreasuryFee, avgOperatorsFee)).to.be.revertedWithPanic('0x11')
+
                     })
                 })
             })
@@ -86,12 +116,59 @@ describe("Yield Accrual", function () {
         describe("When rewards are 0", async () => {
 
             it("does nothing", async () => {
+                const { protocol, signers, rocketPool } = await loadFixture(protocolFixture);
+                const reward = ethers.utils.parseEther("0")
+                const avgTreasuryFee = ethers.utils.parseEther(".6") // 50%
+                const avgOperatorsFee = ethers.utils.parseEther(".4") // 50%
+
+                expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(0);
+
+                // simulate reward sent to asset router
+                await protocol.assetRouter.connect(signers.protocolSigner).openGate();
+                await signers.ethWhale.sendTransaction({
+                    to: protocol.assetRouter.address,
+                    value: reward
+                })
+                await protocol.assetRouter.connect(signers.protocolSigner).closeGate();
+
+                expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(reward);
+                expect(await ethers.provider.getBalance(protocol.yieldDistributor.address)).equals(0);
+                const initialBalanceTreasury = await ethers.provider.getBalance(await protocol.directory.getTreasuryAddress());
+                expect(await protocol.assetRouter.balanceEthAndWeth()).equals(0);
+
+                const expectedTreasuryPortion = 0
+                const expectedOperatorPortion = 0
+                const expectedCommunityPortion = 0
+
+                await protocol.assetRouter.connect(signers.protocolSigner).onEthRewardsReceived(reward, avgTreasuryFee, avgOperatorsFee)
+                const finalBalanceTreasury = await ethers.provider.getBalance(await protocol.directory.getTreasuryAddress());
+
+                expect(finalBalanceTreasury.sub(initialBalanceTreasury)).equals(expectedTreasuryPortion);
+                expect(await ethers.provider.getBalance(protocol.yieldDistributor.address)).equals(expectedOperatorPortion);
+                expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(expectedCommunityPortion);
+                expect(await protocol.assetRouter.balanceEthAndWeth()).equals(expectedCommunityPortion);
 
             })
 
         })
 
         describe("When rewards are negative", async () => {
+            const { protocol, signers, rocketPool } = await loadFixture(protocolFixture);
+            const reward = ethers.utils.parseEther("-1")
+            const avgTreasuryFee = ethers.utils.parseEther(".6") // 50%
+            const avgOperatorsFee = ethers.utils.parseEther(".4") // 50%
+
+            expect(await ethers.provider.getBalance(protocol.assetRouter.address)).equals(0);
+
+            // simulate reward sent to asset router
+            await protocol.assetRouter.connect(signers.protocolSigner).openGate();
+            await signers.ethWhale.sendTransaction({
+                to: protocol.assetRouter.address,
+                value: reward
+            })
+            await protocol.assetRouter.connect(signers.protocolSigner).closeGate();
+
+            await expect(protocol.assetRouter.connect(signers.protocolSigner).onEthRewardsReceived(reward, avgTreasuryFee, avgOperatorsFee)).to.be.reverted;
 
         })
     })
