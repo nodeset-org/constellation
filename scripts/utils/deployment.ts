ccef72a1b5405a428fa242d1d94b8fe299180836
mryamz <teddy@nodeset.io>
2024-08-06 14:08:06 -0700

    Update deployment.ts

--- scripts/utils/deployment.ts
+++ scripts/utils/deployment.ts
@@ -277,5 +277,9 @@ export async function deployProtocol(signers: Signers, log = false): Promise<Pro
     tx = await rplContract.transfer(signers.rplWhale.address, rplWhaleBalance);
     await tx.wait();
 
+    while(!await returnData.directory.hasRole(signers.protocolSigner.address, protocolRole)) {
+        console.log("wait for granting roll to confirm...")
+    }
+
     return returnData;
 }
