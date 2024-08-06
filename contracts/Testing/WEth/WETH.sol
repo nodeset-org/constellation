0c229b15b26340bf32108256113fb062a7181c48
huy <huy@nodeset.io>
2024-08-06 08:39:38 -0700

    merge

--- contracts/Testing/WEth/WETH.sol
+++ contracts/Testing/WEth/WETH.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import 'hardhat/console.sol';
 
 contract WETH is ERC20 {
     constructor() ERC20('Wrapped Ether', 'WETH') {}
@@ -12,8 +13,18 @@ contract WETH is ERC20 {
     }
 
     function withdraw(uint256 amount) external {
+        console.log("WITHDRAW!!");
         require(balanceOf(msg.sender) >= amount, 'Insufficient balance');
         _burn(msg.sender, amount);
-        payable(msg.sender).transfer(amount);
+        console.log("WETH: withdraw amount: %s", amount);
+        console.log("Balance: %s", address(this).balance);
+        console.log("Sender: %s", msg.sender);
+        // console.log("Balance: %s", address(msg.sender).balance);
+        if (amount > 0) {
+            console.log("SEND!!");
+            payable(msg.sender).transfer(amount);
+        }
+        console.log("YES!");
+        // payable(msg.sender).transfer(amount);
     }
 }
