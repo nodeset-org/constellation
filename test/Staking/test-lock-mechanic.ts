958ff5ddca5eddee032b736db74fa77930b28090
mryamz <teddy@nodeset.io>
2024-08-07 13:47:32 -0700

    Update test-lock-mechanic.ts

--- test/Staking/test-lock-mechanic.ts
+++ test/Staking/test-lock-mechanic.ts
@@ -1,15 +1,17 @@
 import { expect } from "chai";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { protocolFixture, SetupData } from ".././test";
-import { approvedSalt, approveHasSignedExitMessageSig, assertAddOperator, prepareOperatorDistributionContract } from "../utils/utils";
-import { generateDepositData } from "../rocketpool/_helpers/minipool";
+import { approvedSalt, approveHasSignedExitMessageSig, assertAddOperator, increaseEVMTime, prepareOperatorDistributionContract } from "../utils/utils";
+import { generateDepositData, generateDepositDataForStake } from "../rocketpool/_helpers/minipool";
 import { BigNumber } from "ethers";
 
-export const prepareToStakeWithLockAmount = async (setupData: SetupData, lockAmount: BigNumber) => {
+const prepareStakeWithLockAmount = async (setupData: SetupData, lockAmount: BigNumber) => {
     const { protocol, signers } = setupData;
     const bond = ethers.utils.parseEther("8");
     const { rawSalt, pepperedSalt } = await approvedSalt(3, signers.hyperdriver.address);
+    expect(await protocol.superNode.hasSufficientLiquidity(bond)).to.equal(false);
     await prepareOperatorDistributionContract(setupData, 2);
+    expect(await protocol.superNode.hasSufficientLiquidity(bond)).to.equal(true);
     await assertAddOperator(setupData, signers.hyperdriver);
     const depositData = await generateDepositData(setupData.protocol.superNode.address, pepperedSalt);
     const config = {
@@ -41,16 +43,18 @@ export const prepareToStakeWithLockAmount = async (setupData: SetupData, lockAmo
 }
 
 describe("Locking Mechanism", async () => {
-    describe("When value is more than lock amount", async () => {
+    describe("When value is equal to lock amount", async () => {
+
         it("should pass", async () => {
             const setupData = await loadFixture(protocolFixture);
             const { protocol, signers } = setupData;
 
             const realLockAmount = await protocol.superNode.lockThreshold();
-            const lockAmount = ethers.utils.parseEther("1.1");
-            expect(lockAmount).gt(realLockAmount);
+            const lockAmount = ethers.utils.parseEther("1");
+            expect(lockAmount).equals(realLockAmount);
 
-            const minipool = await prepareToStakeWithLockAmount(setupData, lockAmount)
+            const minipool = await prepareStakeWithLockAmount(setupData, lockAmount)
+
             const lockedEth = await protocol.superNode.lockedEth(minipool);
             expect(lockedEth).to.equal(lockAmount);
             const totalEthLocked = await protocol.superNode.totalEthLocked();
@@ -58,42 +62,64 @@ describe("Locking Mechanism", async () => {
         })
     })
 
-    describe("When value is equal to lock amount", async () => {
-        const setupData = await loadFixture(protocolFixture);
-        const { protocol, signers } = setupData;
+    describe("When value is more than lock amount", async () => {
+        it("should revert", async () => {
+            const setupData = await loadFixture(protocolFixture);
+            const { protocol, signers } = setupData;
 
-        const realLockAmount = await protocol.superNode.lockThreshold();
-        const lockAmount = ethers.utils.parseEther("1");
-        expect(lockAmount).equal(realLockAmount);
+            const realLockAmount = await protocol.superNode.lockThreshold();
+            const lockAmount = ethers.utils.parseEther("1.001");
+            expect(lockAmount).gt(realLockAmount);
 
-        const minipool = await prepareToStakeWithLockAmount(setupData, lockAmount)
-        const lockedEth = await protocol.superNode.lockedEth(minipool);
-        expect(lockedEth).to.equal(lockAmount);
-        const totalEthLocked = await protocol.superNode.totalEthLocked();
-        expect(totalEthLocked).to.equal(lockAmount);
+            await expect(prepareStakeWithLockAmount(setupData, lockAmount)).to.be.revertedWith("SuperNode: must set the message value to lockThreshold")
+        })
     })
 
     describe("When value is less than lock amount", async () => {
-        const setupData = await loadFixture(protocolFixture);
-        const { protocol, signers } = setupData;
+        it("should revert", async () => {
+            const setupData = await loadFixture(protocolFixture);
+            const { protocol, signers } = setupData;
 
-        const realLockAmount = await protocol.superNode.lockThreshold();
-        const lockAmount = ethers.utils.parseEther(".9");
-        expect(lockAmount).lt(realLockAmount);
+            const realLockAmount = await protocol.superNode.lockThreshold();
+            const lockAmount = ethers.utils.parseEther(".9");
+            expect(lockAmount).lt(realLockAmount);
 
-        const minipool = await expect(prepareToStakeWithLockAmount(setupData, lockAmount)).to.be.revertedWith("adfadf")
+            await expect(prepareStakeWithLockAmount(setupData, lockAmount)).to.be.revertedWith("SuperNode: must set the message value to lockThreshold")
+        })
     })
-
 })
 
 describe("Unlocking Mechanism", async () => {
-
-    describe("When user locked more than locked amount", async () => {
-
-    })
-
     describe("When user locked equal to lock amount", async () => {
+        it("should pass", async () => {
+            const setupData = await loadFixture(protocolFixture);
+            const { protocol, signers } = setupData;
 
+            // lock 1 eth
+            const realLockAmount = await protocol.superNode.lockThreshold();
+            const lockAmount = ethers.utils.parseEther("1");
+            expect(lockAmount).equals(realLockAmount);
+            const minipool = await prepareStakeWithLockAmount(setupData, lockAmount)
+            let lockedEth = await protocol.superNode.lockedEth(minipool);
+            expect(lockedEth).to.equal(lockAmount);
+            let totalEthLocked = await protocol.superNode.totalEthLocked();
+            expect(totalEthLocked).to.equal(lockAmount);
+
+            // prepare to unlock
+            await setupData.rocketPool.rocketDepositPoolContract.deposit({
+                value: ethers.utils.parseEther("32")
+            })
+            await setupData.rocketPool.rocketDepositPoolContract.assignDeposits();
+            await increaseEVMTime(60 * 60 * 24 * 7 * 32);
+            const depositDataStake = await generateDepositDataForStake(minipool);
+
+            // unlock
+            await protocol.superNode.connect(signers.hyperdriver).stake(depositDataStake.depositData.signature, depositDataStake.depositDataRoot, minipool);
+        
+            lockedEth = await protocol.superNode.lockedEth(minipool);
+            expect(lockedEth).to.equal(0);
+            totalEthLocked = await protocol.superNode.totalEthLocked();
+            expect(totalEthLocked).to.equal(0);
+        })
     })
-
 })
