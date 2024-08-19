d792e097b9c9af9b42d60fe8dd2b7b8da4cd3db0
mryamz <teddy@nodeset.io>
2024-08-19 15:53:37 -0700

    Update test-operator-self-remove.ts

--- test/Whitelist/test-operator-self-remove.ts
+++ test/Whitelist/test-operator-self-remove.ts
@@ -7,16 +7,63 @@ import { BigNumber } from "ethers";
 import { keccak256 } from "ethereumjs-util";
 
 describe("Operator Self Remove", function () {
+    describe("When there is 1 user", async () => {
+        describe("When sender has been a sub node operator", async () => {
+            describe("When sender activeValidatorCount is equal to 0", async () => {
+                it("Should pass", async () => {
 
-    describe("When sender activeValidatorCount is equal to 0", async () => {
-        it("Should pass", async () => {
+                })
+            })
 
+            describe("When sender activeValidatorCount is greater than 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
+        })
+
+        describe("When sender has not been a sub node operator", async () => {
+            describe("When sender activeValidatorCount is equal to 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
+
+            describe("When sender activeValidatorCount is greater than 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
         })
     })
 
-    describe("When sender activeValidatorCount is greater than 0", async () => {
-        it("Should pass", async () => {
-            
+    describe("When there are multiple users", async () => {
+        describe("When sender has been a sub node operator", async () => {
+            describe("When sender activeValidatorCount is equal to 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
+
+            describe("When sender activeValidatorCount is greater than 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
+        })
+
+        describe("When sender has not been a sub node operator", async () => {
+            describe("When sender activeValidatorCount is equal to 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
+
+            describe("When sender activeValidatorCount is greater than 0", async () => {
+                it("Should pass", async () => {
+
+                })
+            })
         })
     })
 })
