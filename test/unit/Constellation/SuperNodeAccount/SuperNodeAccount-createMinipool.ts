373636d1afc11253f9def5387b19f4b67649421a
huy <huy@nodeset.io>
2024-09-04 10:00:54 -0700

    mocks

--- test/unit/Constellation/SuperNodeAccount/SuperNodeAccount-createMinipool.ts
+++ test/unit/Constellation/SuperNodeAccount/SuperNodeAccount-createMinipool.ts
@@ -11,6 +11,7 @@ describe("SuperNodeAccount", function () {
   let owner: any;
   let subNodeOperator: any;
   let otherSigner: any;
+  let blockchainId: any;
 
     beforeEach(async function () {
         [owner, subNodeOperator, otherSigner] = await ethers.getSigners();
@@ -37,35 +38,58 @@ describe("SuperNodeAccount", function () {
         });
 
         await superNodeAccount.deployed();
+        blockchainId = await ethers.provider.getNetwork().then((n) => n.chainId);
     });
 
     describe("when the message value is below the threshold", function () {
-        it("should revert", async function () {
+        it.only("should revert", async function () {
             const salts  = await approvedSalt(3, subNodeOperator.address);
             const rawSalt = salts.rawSalt;
             const pepperedSalt = salts.pepperedSalt;
-                const depositData = await generateDepositData(superNodeAccount.address, pepperedSalt)
-            const bond = await setupData.protocol.superNode.bond();
+            const mockDepositData = {
+                depositData: {
+                    pubkey: Buffer.from("8c5e9f4d3b5a7e9d2f6b7c4e8f3d9e1f6c7e4d5b6a3f1c9d8e4f3b6c7a9d5e2b6c4e8f3a9d2e7c1f5d3b7e8c9d6a5", "hex"),
+                    withdrawalCredentials: Buffer.from("00f839d2b469f5b5be987ab46ef4bb26b99e3f7e9c3a6ab4e6e1b4f52edbc293", "hex"),
+                    amount: BigInt(32000000000),
+                    signature: Buffer.from("b8e2d8a4d19f4e73a8f3", "hex"),
+                },
+                depositDataRoot: ethers.utils.arrayify(
+                    "0x0fe98b4ef4b9a2a7cde5a8d9f1c6b7a8e4d3c2b1e8f4d2c3b6a9e7f5b3d8e6f9"
+                ),
+                minipoolAddress: "0xabC1234567890deFAbCdEf1234567890aBcdEF12"
+            }
+            const bond = await superNodeAccount.bond();
             const config = {
                 timezoneLocation: 'Australia/Brisbane',
                 bondAmount: bond,
                 minimumNodeFee: 0,
-                validatorPubkey: depositData.depositData.pubkey,
-                validatorSignature: depositData.depositData.signature,
-                depositDataRoot: depositData.depositDataRoot,
+                validatorPubkey: mockDepositData.depositData.pubkey,
+                validatorSignature: mockDepositData.depositData.signature,
+                depositDataRoot: mockDepositData.depositDataRoot,
                 salt: pepperedSalt,
-                expectedMinipoolAddress: depositData.minipoolAddress,
+                expectedMinipoolAddress: mockDepositData.minipoolAddress,
             };
-            const message = await approveHasSignedExitMessageSig(
-                setupData,
-                nodeOperator.address,
-                '0x' + config.expectedMinipoolAddress,
-                config.salt,
+            const subNodeOperatorNonce = await superNodeAccount.nonces(subNodeOperator.address);
+            const nonce = await superNodeAccount.nonce();
+            const messageHash = ethers.utils.keccak256(
+                ethers.utils.defaultAbiCoder.encode(
+                  ["address", "uint256", "address", "uint256", "uint256", "uint256"],
+                  [
+                    mockDepositData.minipoolAddress,
+                    pepperedSalt,
+                    superNodeAccount.address,
+                    subNodeOperatorNonce,
+                    nonce,
+                    blockchainId,
+                  ]
+                )
             );
-            const sig = message.sig
+            const ethSignedMessageHash = ethers.utils.hashMessage(ethers.utils.arrayify(messageHash));
+            const sig = await subNodeOperator.signMessage(ethers.utils.arrayify(messageHash));
+
             await expect(
-                protocol.superNode
-            .connect(nodeOperator)
+                superNodeAccount
+            .connect(subNodeOperator)
             .createMinipool({
                 validatorPubkey: config.validatorPubkey,
                 validatorSignature: config.validatorSignature,
