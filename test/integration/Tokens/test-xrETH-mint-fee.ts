25f7ca5b8610bc3fd6362510d1764a58c1fae191
Mike Leach (Wander) <6549498+VVander@users.noreply.github.com>
2024-09-06 14:53:47 -0500

    fix test

--- test/integration/Tokens/test-xrETH-mint-fee.ts
+++ test/integration/Tokens/test-xrETH-mint-fee.ts
@@ -17,6 +17,8 @@ describe('xrETH Mint Fee', async function () {
     expect(await protocol.vCWETH.connect(signers.ethWhale).deposit(depositAmount, signers.ethWhale.address)).to.not.be
       .reverted;
 
+    const startingShares = await protocol.vCWETH.balanceOf(signers.ethWhale.address);
+
     // add between 1 and 10 random amount of ETH to TVL
     randValue = Math.floor(Math.random() * 9 + 1);
     console.log('adding', randValue.toString(), 'ETH to TVL');
@@ -37,7 +39,7 @@ describe('xrETH Mint Fee', async function () {
     protocol.wETH.connect(signers.ethWhale).approve(protocol.vCWETH.address, depositAmount);
     expect(await protocol.vCWETH.connect(signers.ethWhale).deposit(depositAmount, signers.ethWhale.address)).to.not.be
       .reverted;
-    const sharesWithDeposit = await protocol.vCWETH.balanceOf(signers.ethWhale.address);
+    const sharesWithDeposit = (await protocol.vCWETH.balanceOf(signers.ethWhale.address)).sub(startingShares);
 
     console.log('redeem', sharesWithDeposit.toString(), 'shares');
     // redeem the shares again to reset xrETH balance
@@ -46,7 +48,7 @@ describe('xrETH Mint Fee', async function () {
         .connect(signers.ethWhale)
         .redeem(sharesWithDeposit, signers.ethWhale.address, signers.ethWhale.address)
     ).to.not.be.reverted;
-    expect(await protocol.vCWETH.balanceOf(signers.ethWhale.address)).to.equal(0);
+    expect(await protocol.vCWETH.balanceOf(signers.ethWhale.address)).to.equal(startingShares);
 
     // Mint 1 share using mint()
     console.log('mint');
@@ -56,7 +58,7 @@ describe('xrETH Mint Fee', async function () {
     protocol.wETH.connect(signers.ethWhale).approve(protocol.vCWETH.address, depositAmount);
     expect(await protocol.vCWETH.connect(signers.ethWhale).mint(ethers.utils.parseEther('1'), signers.ethWhale.address))
       .to.not.be.reverted;
-    const sharesWithMint = await protocol.vCWETH.balanceOf(signers.ethWhale.address);
+    const sharesWithMint = (await protocol.vCWETH.balanceOf(signers.ethWhale.address)).sub(startingShares);
 
     expect(sharesWithDeposit).equals(sharesWithMint);
   });
