f5032aff0a5757100cc9798d68e52cf5fdf11337
huy <huy@nodeset.io>
2024-09-27 08:32:27 -0700

    locaL

--- scripts/submitMerkleClaim_autoupdate.ts
+++ scripts/submitMerkleClaim_autoupdate.ts
@@ -1,13 +1,23 @@
 
+// comment this out for deployment
+require('dotenv').config();
+
 const { Defender } = require('@openzeppelin/defender-sdk');
 const { ethers } = require('ethers');
 const axios = require('axios');
 
+// comment this out for deployment (Defender will provide the credentials)
+// if you're testing, you need these relayer API keys set in your .env file
+const credentials = {
+  relayerApiKey: `${process.env.DEFENDER_RELAY_KEY}`,
+  relayerApiSecret: `${process.env.DEFENDER_RELAY_SECRET}`,
+};
+
 const GITHUB_REPO_API = 'https://api.github.com/repos/rocket-pool/rewards-trees/contents/holesky';
-const GITHUB_TOKEN = 'your_github_token';
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 
-const NODE_ADDRESS = ''
+const NODE_ADDRESS = '0x0b4ead9adb79457816d446deb57d7a22f88c644c'
 
 const ROCKET_STORAGE_ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"oldGuardian","type":"address"},{"indexed":false,"internalType":"address","name":"newGuardian","type":"address"}],"name":"GuardianChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"node","type":"address"},{"indexed":true,"internalType":"address","name":"withdrawalAddress","type":"address"},{"indexed":false,"internalType":"uint256","name":"time","type":"uint256"}],"name":"NodeWithdrawalAddressSet","type":"event"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"addUint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"confirmGuardian","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"confirmWithdrawalAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteBool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteBytes","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteBytes32","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteInt","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteString","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"deleteUint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getAddress","outputs":[{"internalType":"address","name":"r","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getBool","outputs":[{"internalType":"bool","name":"r","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getBytes","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getBytes32","outputs":[{"internalType":"bytes32","name":"r","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getDeployedStatus","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getGuardian","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getInt","outputs":[{"internalType":"int256","name":"r","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodePendingWithdrawalAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeWithdrawalAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getString","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getUint","outputs":[{"internalType":"uint256","name":"r","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"address","name":"_value","type":"address"}],"name":"setAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"bool","name":"_value","type":"bool"}],"name":"setBool","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"bytes","name":"_value","type":"bytes"}],"name":"setBytes","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"bytes32","name":"_value","type":"bytes32"}],"name":"setBytes32","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"setDeployedStatus","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_newAddress","type":"address"}],"name":"setGuardian","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"int256","name":"_value","type":"int256"}],"name":"setInt","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"string","name":"_value","type":"string"}],"name":"setString","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"uint256","name":"_value","type":"uint256"}],"name":"setUint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"address","name":"_newWithdrawalAddress","type":"address"},{"internalType":"bool","name":"_confirm","type":"bool"}],"name":"setWithdrawalAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"subUint","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 const ROCKET_STORAGE_ADDRESS = "0x594Fb75D3dc2DFa0150Ad03F99F97817747dd4E1"
@@ -28,7 +38,7 @@ async function fetchRewardFiles() {
         const response = await axios.get(GITHUB_REPO_API, { headers });
         const files = response.data;
         // Filter and only keep files that are numbers (reward interval files)
-        const rewardFiles = files.filter(file => /^[0-9]+$/.test(file.name));
+        const rewardFiles = files.filter(file => /rp-rewards-holesky-\d+\.json/.test(file.name));
         return rewardFiles;
     } catch (error) {
         console.error('Error fetching files from GitHub:', error);
@@ -37,13 +47,10 @@ async function fetchRewardFiles() {
 }
 
 
-async function fetchRewardFileContent(url) {
-    const headers = GITHUB_TOKEN ? { Authorization: `token ${GITHUB_TOKEN}` } : {};
-
+async function fetchRewardFileContent(downloadUrl) {
     try {
-        const response = await axios.get(url, { headers });
-        const content = Buffer.from(response.data.content, 'base64').toString(); // Decode the base64-encoded content
-        return JSON.parse(content); // Assuming the file is JSON format
+        const response = await axios.get(downloadUrl);
+        return response.data; // Assuming the content is JSON, you can directly return the parsed JSON
     } catch (error) {
         console.error('Error fetching file content from GitHub:', error);
         throw error;
@@ -60,7 +67,7 @@ function processRewardsForVersion(rewardData, version, nodeAddress) {
         case 3:
             return processVersion1(rewardData, nodeAddress);
         default:
-            throw new Error(`Unsupported rewardsFileVersion: ${version}`);
+            return processVersion1(rewardData, nodeAddress);
     }
 }
 
@@ -72,7 +79,12 @@ function processVersion1(rewardData, nodeAddress) {
     return { merkleProof, collateralRpl, smoothingPoolEth };
 }
 
-exports.handler = async function(credentials) {
+//comment this out for deployment
+testFunction(credentials);
+
+// use the exports.handler line for deployment, use the other one for testing
+// exports.handler = async function(credentials) {
+async function testFunction(credentials: any) {
     const client = new Defender(credentials);
 
     const provider = client.relaySigner.getProvider();
@@ -90,20 +102,22 @@ exports.handler = async function(credentials) {
     try {
         // Fetch reward files from GitHub
         const rewardFiles = await fetchRewardFiles();
-        console.log('Fetched reward files:', rewardFiles.map(file => file.name));
+        console.log('Fetched reward files:', rewardFiles.length);
 
         // Loop over each reward file and collect data for all claims
         for (const file of rewardFiles) {
+            console.log(`Processing file: ${file.name}`);
             // Extract interval from filenames like 'rp-rewards-holesky-124.json' or 'rp-rewards-holesky-999.json'
             const intervalMatch = file.name.match(/rp-rewards-holesky-(\d+)\.json/);
             if (!intervalMatch) {
                 console.log(`Invalid filename format: ${file.name}, skipping...`);
                 continue; // Skip if the filename doesn't match the expected pattern
             }
-
+            console.log("HUY 1")
             const interval = parseInt(intervalMatch[1], 10); // Extracted interval number
             const indexWordIndex = Math.floor(interval / 256); // Get the word index
             const indexBitIndex = interval % 256; // Get the bit index within that word
+            console.log("HUY 2")
 
             // Create the key for checking whether the reward has been claimed
             const claimedWordKey = ethers.utils.keccak256(
@@ -112,9 +126,11 @@ exports.handler = async function(credentials) {
                     ['rewards.interval.claimed', NODE_ADDRESS, indexWordIndex]
                 )
             );
+            console.log("HUY 3")
 
             // Get the claimedWord for this interval
             const claimedWord = await rocketStorage.getUint(claimedWordKey);
+            console.log("HUY 4")
 
             // Check if the reward has been claimed
             if (isClaimed(claimedWord, indexBitIndex)) {
@@ -151,21 +167,34 @@ exports.handler = async function(credentials) {
             try {
                 console.log(`Submitting batch Merkle claim for ${rewardIndexes.length} intervals...`);
 
-                const txResult = await merkleClaimStreamer.submitMerkleClaim(
+                // use the callStatic line for local testing, the other for deployment
+                const txResult = await merkleClaimStreamer.callStatic.submitMerkleClaim(
                     rewardIndexes,
                     amountsRPL,
                     amountsETH,
-                    merkleProofsArray
+                    merkleProofsArray,
+                    { maxFeePerGas: 200, gasLimit: 1000000 }
                 );
+                // const txResult = await merkleClaimStreamer.submitMerkleClaim(
+                //     rewardIndexes,
+                //     amountsRPL,
+                //     amountsETH,
+                //     merkleProofsArray,
+                //     { maxFeePerGas: 200, gasLimit: 1000000 }
+                // );
 
-                console.log(`Successfully claimed rewards for ${rewardIndexes.length} intervals, tx: ${txResult.hash}`);
+                console.log(txResult);
+
+                // uncomment this for deployment
+                // return txResult.hash;
             } catch (error) {
-                console.error(`Failed to submit Merkle claim:`, error);
+                console.log(`Failed to submit Merkle claim:`, error);
             }
         } else {
             console.log('No unclaimed rewards to submit.');
         }
     } catch (error) {
-        console.error('Error processing rewards:', error);
+        console.log('Error processing rewards:', error);
     }
+
 };
