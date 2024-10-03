65c939627bdba527f0e3241e874aea114a9c5afd
Mike Leach (Wander) <6549498+VVander@users.noreply.github.com>
2024-10-03 17:43:35 -0500

    add script to deploy bare implementation and encode it in an upgradeTo call

--- scripts/environments/timelock.ts
+++ scripts/environments/timelock.ts
@@ -1,27 +1,30 @@
-import findConfig from "find-config";
-import dotenv from "dotenv";
-import { getWalletFromPath } from "./keyReader";
+import findConfig from 'find-config';
+import dotenv from 'dotenv';
+import { getWalletFromPath } from '../utils/keyReader';
 
-export async function deployTimelockFromEnv(hre: any, env: string, log: boolean, minDelaySeconds: any, proposers: string[], executors: string[]) {
-    const dotenvPath = findConfig(`.env.${env}`);
-    if (dotenvPath !== null) {
-      dotenv.config({ path: dotenvPath });
-    } else {
-      console.error("File ", `.env.${env} could not be found`)
-    }
+export async function deployTimelockFromEnv(
+  hre: any,
+  env: string,
+  log: boolean,
+  minDelaySeconds: any,
+  proposers: string[],
+  executors: string[]
+) {
+  const dotenvPath = findConfig(`.env.${env}`);
+  if (dotenvPath !== null) {
+    dotenv.config({ path: dotenvPath });
+  } else {
+    console.error('File ', `.env.${env} could not be found`);
+  }
 
-    const deployerWallet = await getWalletFromPath(hre.ethers, process.env.DEPLOYER_PRIVATE_KEY_PATH as string);
+  const deployerWallet = await getWalletFromPath(hre.ethers, process.env.DEPLOYER_PRIVATE_KEY_PATH as string);
 
-    const Timelock = await hre.ethers.getContractFactory("TimelockController", deployerWallet)
-    const timelock = await Timelock.deploy(
-      minDelaySeconds,
-      proposers,
-      executors
-    );
+  const Timelock = await hre.ethers.getContractFactory('TimelockController', deployerWallet);
+  const timelock = await Timelock.deploy(minDelaySeconds, proposers, executors);
 
-    if(log) {
-        console.log(minDelaySeconds.toString(), "second timelock deployed to", timelock.address);
-    }
+  if (log) {
+    console.log(minDelaySeconds.toString(), 'second timelock deployed to', timelock.address);
+  }
 
-    return timelock.address
+  return timelock.address;
 }
--- tasks/timelockTasks.ts
+++ tasks/timelockTasks.ts
@@ -1,124 +1,157 @@
+import { task, types } from 'hardhat/config';
+import findConfig from 'find-config';
+import { getWalletFromPath } from '../scripts/utils/keyReader';
+import dotenv from 'dotenv';
+import { ContractFactory } from 'ethers';
+import { deployContract } from '@nomiclabs/hardhat-ethers/types';
 
-import { task, types } from "hardhat/config";
+task('upgradeTo', 'Encodes the upgradeTo(address) function call for an upgradable contract')
+  .addParam('newImplementation', 'The address of the new implementation contract', undefined, types.string)
+  .setAction(async ({ newImplementation }, hre) => {
+    const sigs = ['upgradeTo(address)'];
+    const params = [[newImplementation]];
 
-task("upgradeTo", "Encodes the upgradeTo(address) function call for an upgradable contract")
-    .addParam("newImplementation", "The address of the new implementation contract", undefined, types.string)
-    .setAction(async ({ newImplementation }, hre) => {
-        const sigs = ["upgradeTo(address)"];
-        const params = [[newImplementation]];
+    console.log(`Encoding upgradeTo with new implementation: ${newImplementation}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-        console.log(`Encoding upgradeTo with new implementation: ${newImplementation}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+task(
+  'deployAndEncodeUpgrade',
+  'Deploys a new implementation contract and encodes the upgradeTo(address) function call for an upgradable contract'
+)
+  .addParam('contractName', 'The name of the contract', undefined, types.string)
+  .addParam('environmentName', 'The name of the env file to use (.environmentName.env)', undefined, types.string)
+  .setAction(async ({ contractName, environmentName }, hre) => {
+    const dotenvPath = findConfig(`.${environmentName}.env`);
+    if (dotenvPath !== null) {
+      dotenv.config({ path: dotenvPath });
+    } else {
+      // Handle the case where no .env file is found
+      console.error('No .env.' + environmentName + 'file found');
+      return;
+    }
+    const deployerWallet = await getWalletFromPath(ethers, process.env.DEPLOYER_PRIVATE_KEY_PATH as string);
 
-task("upgradeToAndCall", "Encodes the upgradeToAndCall(address,bytes) function call for an upgradable contract")
-    .addParam("newImplementation", "The address of the new implementation contract", undefined, types.string)
-    .addParam("data", "The calldata to be executed after the upgrade", undefined, types.string)
-    .setAction(async ({ newImplementation, data }, hre) => {
-        const sigs = ["upgradeToAndCall(address,bytes)"];
-        const params = [[newImplementation, data]];
+    console.log(`Deploying new implementation contract for ${contractName}...`);
+    const contract = await (await hre.ethers.deployContract(contractName, [], deployerWallet)).deployed();
 
-        console.log(`Encoding upgradeToAndCall with new implementation: ${newImplementation} and data: ${data}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Deployed new implementation contract for ${contractName}: ${contract.address}`);
 
+    return await hre.run('upgradeTo', { newImplementation: contract.address });
+  });
 
-task("setTreasuryFeeRplVault", "Encodes the setTreasuryFee(uint256) function call for RPLVault")
-    .addParam("treasuryFee", "The new treasury fee (uint256)", undefined, types.string)
-    .setAction(async ({ treasuryFee }, hre) => {
-        const sigs = ["setTreasuryFee(uint256)"];
-        const params = [[treasuryFee]];
+task('upgradeToAndCall', 'Encodes the upgradeToAndCall(address,bytes) function call for an upgradable contract')
+  .addParam('newImplementation', 'The address of the new implementation contract', undefined, types.string)
+  .addParam('data', 'The calldata to be executed after the upgrade', undefined, types.string)
+  .setAction(async ({ newImplementation, data }, hre) => {
+    const sigs = ['upgradeToAndCall(address,bytes)'];
+    const params = [[newImplementation, data]];
 
-        console.log(`Encoding setTreasuryFee for RPLVault with fee: ${treasuryFee}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding upgradeToAndCall with new implementation: ${newImplementation} and data: ${data}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setTreasuryFeeWETHVault", "Encodes the setTreasuryFee(uint256) function call for WETHVault")
-    .addParam("treasuryFee", "The new treasury fee (uint256)", undefined, types.string)
-    .setAction(async ({ treasuryFee }, hre) => {
-        const sigs = ["setTreasuryFee(uint256)"];
-        const params = [[treasuryFee]];
+task('setTreasuryFeeRplVault', 'Encodes the setTreasuryFee(uint256) function call for RPLVault')
+  .addParam('treasuryFee', 'The new treasury fee (uint256)', undefined, types.string)
+  .setAction(async ({ treasuryFee }, hre) => {
+    const sigs = ['setTreasuryFee(uint256)'];
+    const params = [[treasuryFee]];
 
-        console.log(`Encoding setTreasuryFee for WETHVault with fee: ${treasuryFee}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setTreasuryFee for RPLVault with fee: ${treasuryFee}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setNodeOperatorFee", "Encodes the setNodeOperatorFee(uint256) function call for WETHVault")
-    .addParam("nodeOperatorFee", "The new node operator fee (uint256)", undefined, types.string)
-    .setAction(async ({ nodeOperatorFee }, hre) => {
-        const sigs = ["setNodeOperatorFee(uint256)"];
-        const params = [[nodeOperatorFee]];
+task('setTreasuryFeeWETHVault', 'Encodes the setTreasuryFee(uint256) function call for WETHVault')
+  .addParam('treasuryFee', 'The new treasury fee (uint256)', undefined, types.string)
+  .setAction(async ({ treasuryFee }, hre) => {
+    const sigs = ['setTreasuryFee(uint256)'];
+    const params = [[treasuryFee]];
 
-        console.log(`Encoding setNodeOperatorFee for WETHVault with fee: ${nodeOperatorFee}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setTreasuryFee for WETHVault with fee: ${treasuryFee}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setProtocolFees", "Encodes the setProtocolFees(uint256,uint256) function call for WETHVault")
-    .addParam("nodeOperatorFee", "The new node operator fee (uint256)", undefined, types.string)
-    .addParam("treasuryFee", "The new treasury fee (uint256)", undefined, types.string)
-    .setAction(async ({ nodeOperatorFee, treasuryFee }, hre) => {
-        const sigs = ["setProtocolFees(uint256,uint256)"];
-        const params = [[nodeOperatorFee, treasuryFee]];
+task('setNodeOperatorFee', 'Encodes the setNodeOperatorFee(uint256) function call for WETHVault')
+  .addParam('nodeOperatorFee', 'The new node operator fee (uint256)', undefined, types.string)
+  .setAction(async ({ nodeOperatorFee }, hre) => {
+    const sigs = ['setNodeOperatorFee(uint256)'];
+    const params = [[nodeOperatorFee]];
 
-        console.log(`Encoding setProtocolFees for WETHVault with nodeOperatorFee: ${nodeOperatorFee} and treasuryFee: ${treasuryFee}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setNodeOperatorFee for WETHVault with fee: ${nodeOperatorFee}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setMintFee", "Encodes the setMintFee(uint256) function call for WETHVault")
-    .addParam("newMintFee", "The new mint fee (uint256)", undefined, types.string)
-    .setAction(async ({ newMintFee }, hre) => {
-        const sigs = ["setMintFee(uint256)"];
-        const params = [[newMintFee]];
+task('setProtocolFees', 'Encodes the setProtocolFees(uint256,uint256) function call for WETHVault')
+  .addParam('nodeOperatorFee', 'The new node operator fee (uint256)', undefined, types.string)
+  .addParam('treasuryFee', 'The new treasury fee (uint256)', undefined, types.string)
+  .setAction(async ({ nodeOperatorFee, treasuryFee }, hre) => {
+    const sigs = ['setProtocolFees(uint256,uint256)'];
+    const params = [[nodeOperatorFee, treasuryFee]];
 
-        console.log(`Encoding setMintFee for WETHVault with newMintFee: ${newMintFee}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(
+      `Encoding setProtocolFees for WETHVault with nodeOperatorFee: ${nodeOperatorFee} and treasuryFee: ${treasuryFee}`
+    );
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setLiquidityReservePercentWETHVault", "Encodes the setLiquidityReservePercent(uint256) function call for WETHVault")
-    .addParam("liquidityReservePercent", "The new liquidity reserve percent (uint256)", undefined, types.string)
-    .setAction(async ({ liquidityReservePercent }, hre) => {
-        const sigs = ["setLiquidityReservePercent(uint256)"];
-        const params = [[liquidityReservePercent]];
+task('setMintFee', 'Encodes the setMintFee(uint256) function call for WETHVault')
+  .addParam('newMintFee', 'The new mint fee (uint256)', undefined, types.string)
+  .setAction(async ({ newMintFee }, hre) => {
+    const sigs = ['setMintFee(uint256)'];
+    const params = [[newMintFee]];
 
-        console.log(`Encoding setLiquidityReservePercent for MerkleClaimStreamer with percent: ${liquidityReservePercent}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setMintFee for WETHVault with newMintFee: ${newMintFee}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setMinWethRplRatio", "Encodes the setMinWethRplRatio(uint256) function call for RPLVault")
-    .addParam("minWethRplRatio", "The new minimum WETH/RPL ratio (uint256)", undefined, types.string)
-    .setAction(async ({ minWethRplRatio }, hre) => {
-        const sigs = ["setMinWethRplRatio(uint256)"];
-        const params = [[minWethRplRatio]];
+task(
+  'setLiquidityReservePercentWETHVault',
+  'Encodes the setLiquidityReservePercent(uint256) function call for WETHVault'
+)
+  .addParam('liquidityReservePercent', 'The new liquidity reserve percent (uint256)', undefined, types.string)
+  .setAction(async ({ liquidityReservePercent }, hre) => {
+    const sigs = ['setLiquidityReservePercent(uint256)'];
+    const params = [[liquidityReservePercent]];
 
-        console.log(`Encoding setMinWethRplRatio for RPLVault with ratio: ${minWethRplRatio}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
-task("setLiquidityReservePercentRPLVault", "Encodes the setLiquidityReservePercent(uint256) function call for RPLVault")
-    .addParam("liquidityReservePercent", "The new liquidity reserve percent (uint256)", undefined, types.string)
-    .setAction(async ({ liquidityReservePercent }, hre) => {
-        const sigs = ["setLiquidityReservePercent(uint256)"];
-        const params = [[liquidityReservePercent]];
+    console.log(`Encoding setLiquidityReservePercent for MerkleClaimStreamer with percent: ${liquidityReservePercent}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-        console.log(`Encoding setLiquidityReservePercent for RPLVault with percent: ${liquidityReservePercent}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+task('setMinWethRplRatio', 'Encodes the setMinWethRplRatio(uint256) function call for RPLVault')
+  .addParam('minWethRplRatio', 'The new minimum WETH/RPL ratio (uint256)', undefined, types.string)
+  .setAction(async ({ minWethRplRatio }, hre) => {
+    const sigs = ['setMinWethRplRatio(uint256)'];
+    const params = [[minWethRplRatio]];
 
-task("setLockAmount", "Encodes the setLockAmount(uint256) function call for SuperNodeAccount")
-    .addParam("newLockThreshold", "The new lock threshold amount (uint256)", undefined, types.string)
-    .setAction(async ({ newLockThreshold }, hre) => {
-        const sigs = ["setLockAmount(uint256)"];
-        const params = [[newLockThreshold]];
+    console.log(`Encoding setMinWethRplRatio for RPLVault with ratio: ${minWethRplRatio}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
+task('setLiquidityReservePercentRPLVault', 'Encodes the setLiquidityReservePercent(uint256) function call for RPLVault')
+  .addParam('liquidityReservePercent', 'The new liquidity reserve percent (uint256)', undefined, types.string)
+  .setAction(async ({ liquidityReservePercent }, hre) => {
+    const sigs = ['setLiquidityReservePercent(uint256)'];
+    const params = [[liquidityReservePercent]];
 
-        console.log(`Encoding setLockAmount for SuperNodeAccount with threshold: ${newLockThreshold}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setLiquidityReservePercent for RPLVault with percent: ${liquidityReservePercent}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
 
-task("setMaxWethRplRatio", "Encodes the setMaxWethRplRatio(uint256) function call for WETHVault")
-    .addParam("maxWethRplRatio", "The new maximum WETH/RPL ratio (uint256)", undefined, types.string)
-    .setAction(async ({ maxWethRplRatio }, hre) => {
-        const sigs = ["setMaxWethRplRatio(uint256)"];
-        const params = [[maxWethRplRatio]];
+task('setLockAmount', 'Encodes the setLockAmount(uint256) function call for SuperNodeAccount')
+  .addParam('newLockThreshold', 'The new lock threshold amount (uint256)', undefined, types.string)
+  .setAction(async ({ newLockThreshold }, hre) => {
+    const sigs = ['setLockAmount(uint256)'];
+    const params = [[newLockThreshold]];
 
-        console.log(`Encoding setMaxWethRplRatio for WETHVault with ratio: ${maxWethRplRatio}`);
-        return await hre.run("encodeProposal", { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
-    });
+    console.log(`Encoding setLockAmount for SuperNodeAccount with threshold: ${newLockThreshold}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
+
+task('setMaxWethRplRatio', 'Encodes the setMaxWethRplRatio(uint256) function call for WETHVault')
+  .addParam('maxWethRplRatio', 'The new maximum WETH/RPL ratio (uint256)', undefined, types.string)
+  .setAction(async ({ maxWethRplRatio }, hre) => {
+    const sigs = ['setMaxWethRplRatio(uint256)'];
+    const params = [[maxWethRplRatio]];
+
+    console.log(`Encoding setMaxWethRplRatio for WETHVault with ratio: ${maxWethRplRatio}`);
+    return await hre.run('encodeProposal', { sigs: JSON.stringify(sigs), params: JSON.stringify(params) });
+  });
