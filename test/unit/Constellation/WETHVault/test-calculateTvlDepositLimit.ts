1822b763bd01ba7dff2d74a60b4c427ac0c2607e
huy <huy@nodeset.io>
2024-11-05 10:52:06 -0800

    Add source of truth ascii

--- test/unit/Constellation/WETHVault/test-calculateTvlDepositLimit.ts
+++ test/unit/Constellation/WETHVault/test-calculateTvlDepositLimit.ts
@@ -4,6 +4,23 @@ import { Contract } from "ethers";
 
 const AdminRole = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("ADMIN_ROLE"));
 
+/* Source of Truth
++------------------------+----------------+-----------------------+--------------------------+------------------------------+--------------------------+
+| Scenarios              | ETH in rETH DP | Constellation ETH TVL | Operator Distributor ETH | queueableDepositPercentLimit | Max WETH Deposit Amount  |
+|                        |                |                       | Balance                  |                              |                          |
++------------------------+----------------+-----------------------+--------------------------+------------------------------+--------------------------+
+| Normal Conditions      |       24       |          100          |            8             |             1%               |           1              |
+| Zero Percent Limit     |       50       |          100          |            8             |             0%               |         8.66...          |
+| No rETH Available      |       0        |          100          |            8             |             1%               |           1              |
+| Healthy rETH Ratio     |       72       |          100          |           24             |            10%               |           10             |
+| Low Constellation ETH  |       72       |          24           |           24             |            10%               |           2.4            |
+| No ETH in OD           |       72       |          0            |            0             |            10%               |           24             |
+| High Constellation ETH |       72       |         1000          |            0             |            10%               |          124             |
++------------------------+----------------+-----------------------+--------------------------+------------------------------+--------------------------+
+
+*/
+
+
 describe("WETHVault.calculateTvlDepositLimit", function () {
     let wethVault: Contract;
     let mockDirectory: Contract;
